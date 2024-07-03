c67442a6b299eabb4e2123bc710a1481140f0ab2
vasudev kumawat <130001057+vasudev200@users.noreply.github.com>
2024-07-04 01:22:34 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-let menuLink = document.querySelector(".menu-link");
-let navList = document.querySelector(".nav-link");
-menuLink.addEventListener("click", function () {
-  navList.classList.toggle("active");
-});
